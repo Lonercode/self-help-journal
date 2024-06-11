08d0fcfdf11611487c62fce5536c91afcc3e4bae
lonercode <penpalclassmate@gmail.com>
2024-06-11 16:45:40 +0100

    in production

--- Frontend/Otu-Heart/src/App.jsx
+++ Frontend/Otu-Heart/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Home from './pages/home'
 import './App.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
