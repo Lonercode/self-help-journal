11d00d2460406c5bbb02f0fa19b2cb40e5c0084b
lonercode <penpalclassmate@gmail.com>
2024-06-22 02:58:14 +0100

    frontend

--- Frontend/Otu-Heart/src/pages/dashboard.jsx
+++ Frontend/Otu-Heart/src/pages/dashboard.jsx
@@ -12,6 +12,62 @@ import dateFormat from 'dateformat'
 const config = {
   headers: {Authorization: `Bearer ${token}`}
 }
+
+
+const UserEntries = () => {
+  const [entries, setEntries] = useState([]);
+
+const handleId = (id) => {
+  cookies.set('entryId', id, {
+    path: '/entryPage'
+  })
+}
+    axios.get('/api/otu-heart/myEntries', config)
+    .then((res) => setEntries(res.data.message))
+  
+  
+  
+  let val = entries.map((item) => {
+    const dateTime =  dateFormat(`${item.date}`, "mmmm dS, yyyy")
+    const imageLink = item.image
+   
+    return (<>
+    <div className = "entry" key={item._id}>
+    <img src={imageLink}/>
+    <h3>{item.title}</h3>
+    <p id ="dateTime">{dateTime}</p><br/>
+    <p>{item.content.substring(0, 100)}...</p>
+    <a href = "/entryPage" onClick={handleId(item._id)}>Read my thoughts</a>
+    </div>
+    </>)
+  })
+
+  
+
+  if (val.length != 0){
+    return (
+        <>
+        <div className='stuff'>
+                {val}
+        </div>
+        </>
+    )
+  }
+
+  else{
+   
+    return (
+      <>
+      <div className='none'>
+              <p>You have no entries listed. Please create a new entry.</p>
+      </div>
+      </>
+  )
+}
+}
+
+
+
   
 
 function Dashboard(){
@@ -23,66 +79,6 @@ function Dashboard(){
       navigate('/')
     
   }
-
-  const handleId = (id) => {
-    cookies.set('entryId', id, {
-      path: '/entryPage'
-    })
-    
-      navigate("/entryPage");
-       
-  
-  }
-
-  const UserEntries = () => {
-    const [entries, setEntries] = useState([]);
-  
-  
-      axios.get('/api/otu-heart/myEntries', config)
-      .then((res) => setEntries(res.data.message))
-    
-    
-    
-    let val = entries.map((item) => {
-      const dateTime =  dateFormat(`${item.date}`, "mmmm dS, yyyy")
-      const imageLink = item.image
-     
-      return (<>
-      <div className = "entry" key={item._id}>
-      <img src={imageLink}/>
-      <h3>{item.title}</h3>
-      <p id ="dateTime">{dateTime}</p><br/>
-      <p>{item.content.substring(0, 100)}...</p>
-      <button type = "button" onClick={handleId(item._id)}>Read my thoughts</button>
-      </div>
-      </>)
-    })
-  
-    
-  
-    if (val.length != 0){
-      return (
-          <>
-          <div className='stuff'>
-                  {val}
-          </div>
-          </>
-      )
-    }
-  
-    else{
-     
-      return (
-        <>
-        <div className='none'>
-                <p>You have no entries listed. Please create a new entry.</p>
-        </div>
-        </>
-    )
-  }
-  }
-  
-  
   
     return (
         <>
