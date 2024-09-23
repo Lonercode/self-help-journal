54bb50597637176f5e9334e82dcc5ab102226445
lonercode <penpalclassmate@gmail.com>
2024-09-23 17:16:19 +0100

    joi validation for entrySchema

--- Backend/models/entry.models.js
+++ Backend/models/entry.models.js
@@ -1,41 +1,20 @@
 const mongoose = require('mongoose')
 const entrySchema = mongoose.Schema
 const auth = require('../models/auth.models')
+const joi = require('joi')
+const joigoose = require('joigoose')
 
+const joiSchema = joi.object({
+    imagePublicUrl: joi.string().uri().optional(),
+    image: joi.string().required(),
+    title: joi.string().required().unique(),
+    content: joi.string().required(),
+    user: joi.string().required(),
+    date: joi.date().default(Date.now())
+})
 
-const entryModel = new entrySchema({
+const entryModel = new entrySchema(joigoose.convert(joiSchema), {timestamps: true})
 
-    imagePublicUrl: {
-        type: String,
-        required: false
-    },
-
-    image: {
-        type: String,
-        required: true
-    },
-
-    title: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    content: {
-        type: String,
-        required: true
-    },
-   
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'auth',
-   },
-   date: {
-    type: Date,
-    default: Date.now()
-   }
-   
-}, {timestamps: true}
-)
+entryModel.path('user').ref('auth')
 
 module.exports = mongoose.model('entry', entryModel)
